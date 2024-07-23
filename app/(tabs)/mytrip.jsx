68f0f1e096f09ec11a57032be48d7cb08c1503d5
Mahesh2056 <maheschy2323@gmail.com>
2024-07-24 01:27:27 +0545

    Design Home Page (mytrip)

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Colors } from '../../constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import StartNewTripCard from '../../components/MyTrips/StartNewTripCard';
 
 export default function MyTrip() {
+
+  const [userTrips,setUserTrips] = useState([]);
+
   return (
-    <View>
-      <Text>MyTrip</Text>
+    <View style={{
+      padding: 25,
+      paddingTop: 55,
+      backgroundColor: Colors.WHITE,
+      height: "100%"
+    }}>
+
+      <View style={{
+        display: "flex",
+        flexDirection: "row",
+        alignContent: "center",
+        justifyContent: "space-between"
+      }}>
+        <Text style={{
+          fontFamily: "outfit-bold",
+          fontSize: 35
+        }}>
+          My Trips
+        </Text>
+        <Ionicons name="add-circle" size={50} color="black" />
+      </View>
+
+      {userTrips?.length == 0?
+        <StartNewTripCard />
+        :null
+      }
     </View>
   )
 }
---
+++ components/MyTrips/StartNewTripCard.jsx
@@ -0,0 +1,58 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../../constants/Colors";
+
+export default function StartNewTripCard() {
+  return (
+    <View
+      style={{
+        padding: 20,
+        marginTop: 50,
+        display: "flex",
+        alignItems: "center",
+        gap: 25,
+      }}
+    >
+      <Ionicons name="location-sharp" size={30} color="black" />
+      <Text
+        style={{
+          fontSize: 25,
+          fontFamily: "outfit-medium",
+        }}
+      >
+        No trips planned yet
+      </Text>
+
+      <Text
+        style={{
+          fontSize: 20,
+          fontFamily: "outfit",
+          textAlign: "center",
+          color: Colors.GRAY,
+        }}
+      >
+        Looks like its time to plan a new travel experince! Get Started below
+      </Text>
+
+      <TouchableOpacity
+        style={{
+          padding: 15,
+          backgroundColor: Colors.PRIMARY,
+          borderRadius: 15,
+          paddingHorizontal: 30,
+        }}
+      >
+        <Text
+          style={{
+            color: Colors.WHITE,
+            fontFamily: "outfit-medium",
+            fontSize: 17,
+          }}
+        >
+          Start a new trip
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
