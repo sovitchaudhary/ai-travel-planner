ced58376e63928434915285aaf5d811006fd64de
Mahesh2056 <maheschy2323@gmail.com>
2024-07-24 00:29:07 +0545

    Add My Trip Icon

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,22 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import {Colors} from "./../../constants/Colors";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="mytrip" />
+    <Tabs screenOptions={{ 
+        headerShown: false,
+        tabBarActiveTintColor: Colors.PRIMARY
+    }}>
+      <Tabs.Screen name="mytrip"
+        options={{
+          tabBarLabel:"My Trip",
+          tabBarIcon:({color})=><Ionicons name="location-sharp" 
+          size={24} color={color} />
+        }}
+      />
       <Tabs.Screen name="discover" />
       <Tabs.Screen name="profile" />
     </Tabs>
